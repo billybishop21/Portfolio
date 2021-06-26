4144daa6f26ea54bbfb81f897b7b9d712a057920
Billy <billy.bishop@rice.edu>
2021-06-26 11:32:32 -0500

    commit part 5 of loan_analyzer

--- loan_analyzer/loan_analyzer.py
+++ loan_analyzer/loan_analyzer.py
@@ -206,6 +206,9 @@ output_path = Path("inexpensive_loans.csv")
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
-# YOUR CODE HERE!
-# test 123
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(header)
+    for row in loans:
+        csvwriter.writerow(loan.values())
 
